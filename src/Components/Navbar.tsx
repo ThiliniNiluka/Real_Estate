40068c783b01c01f4e5db1cdff53263a28c3bcf1
ThiliniNiluka <thilininilukakumari@gamil.com>
2025-01-22 07:04:16 +0530

    create navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,31 @@
+import logo from "../assets/logo.svg";
+
+export default function Navbar() {
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
+        <img src={logo} alt="" />
+        <ul className="hidden md:flex gap-7 text-white">
+          <a href="/Home" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="/About" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="/Project" className="cursor-pointer hover:text-gray-400">
+            Project
+          </a>
+          <a
+            href="/Testimonials"
+            className="cursor-pointer hover:text-gray-400"
+          >
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block px-8 bg-white py-2 rounded-full">
+          Sign up
+        </button>
+      </div>
+    </div>
+  );
+}
